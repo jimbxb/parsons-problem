cd1db8d2a24fa192c3acf81e9b2b65cca83fd3c1
James Barnes <jamesbarnes2505@gmail.com>
2022-12-25 06:43:28 +1100

    added dynamic programming approach

--- Parsons.hs
+++ Parsons.hs
@@ -8,6 +8,7 @@ Stability   : experimental
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
@@ -18,8 +19,8 @@ import Control.Exception (evaluate)
 import Control.Monad (foldM, join, liftM2)
 import Data.Either (partitionEithers)
 import Data.Function (on)
-import Data.List (sort, sortBy)
-import Data.Maybe (catMaybes, fromMaybe, isNothing, maybe)
+import Data.List ((\\), foldl', sort, sortBy)
+import Data.Maybe (catMaybes, fromJust, fromMaybe, isNothing, maybe)
 import GHC.Generics (Generic)
 import System.CPUTime (getCPUTime)
 import System.Directory (doesFileExist)
@@ -31,7 +32,7 @@ import Text.Read (readMaybe)
 data Edit
   = Swap Int Int
   | Indent Int Int
-  | Cycle Int Int
+  | Move Int
   | Insert Int
   | Delete Int
   deriving (Eq, Ord, Show, Generic)
@@ -42,91 +43,143 @@ maxTime :: Integer
 maxTime = 59 * 10 ^ 12
 
 main :: IO ()
--- main = grok
 main = do
   argv <- getArgs
   case (argv, readMaybe $ head argv) of
+    ([], _) -> grok
     (_:ans:solns, Just max) -> do
-      exists <- mapM doesFileExist (ans : solns)
-      if and exists
+      exists <- and <$> mapM doesFileExist (ans : solns)
+      if exists
         then do
-          mbEdits <- parsons max ans solns
+          mbEdits <- parsonsDP max ans solns
           case mbEdits of
             Nothing -> do
               print 0
               putStrLn "fail"
-            Just edits -> do
+            Just (ansLines, solnLines, edits) -> do
               print $ max - length edits
               print edits
+              putStr (explain ansLines solnLines edits)
         else exitFailure
     _ -> exitFailure
 
 grok :: IO ()
 grok = do
-  let max = 10
+  let maxEdits = 10
   let ans = "answer.txt"
   let solns = ["soln1.txt"]
   let maxTime = 60 * 10 ^ 6
+  let solver = parsonsDP
   secret <- getEnv "CHECKER_SECRET"
-  mbEdits <- join <$> timeout maxTime (parsons max ans solns)
+  mbEdits <- join <$> timeout maxTime (solver maxEdits ans solns)
   case mbEdits of
     Nothing ->
       putStrLn $
       "{ \"secret\": " ++
       show secret ++ ", \"score\": 0, \"output_msg\": \"too many edits\" }"
-    Just edits ->
-      putStrLn $
-      "{ \"secret\": " ++
-      show secret ++
-      ", \"score\": " ++
-      show (max - length edits) ++ ", \"output_msg\": " ++ show edits ++ " }"
+    Just (ansLines, solnLines, edits) ->
+      let score =
+            max 0 $
+            fromIntegral (maxEdits - length edits) `div`
+            fromIntegral (length edits)
+       in putStrLn $
+          "{ \"secret\": " ++
+          show secret ++
+          ", \"score\": " ++
+          show score ++
+          ", \"output_msg\": " ++
+          show (explain ansLines solnLines edits) ++ " }"
 
-parsons :: Int -> String -> [String] -> IO (Maybe [Edit])
-parsons max ans solns = do
+parsons ::
+     (Int -> [Int] -> [Maybe (Int, Int)] -> Maybe [Edit])
+  -> Int
+  -> FilePath
+  -> [FilePath]
+  -> IO (Maybe ([String], [String], [Edit]))
+parsons solver maxEdits ans solns = do
   (ans':solns') <- map lines <$> mapM readFile (ans : solns)
   let preprocessed = concatMap (preprocess ans') solns'
   t0 <- getCPUTime
-  foldM (go t0) Nothing $ reverse $ zip [1 ..] preprocessed
+  let go curr (remaining, (solnLines, soln, ans)) = do
+        t <- getCPUTime
+        let remTime = (maxTime - (t - t0)) `div` (remaining * 10 ^ 6)
+        next <-
+          join <$>
+          timeout
+            (fromInteger $ min maxTime remTime)
+            (forceEval $ solver (maybe maxEdits (length . thd) curr) soln ans)
+        return $ combine ((ans', solnLines, ) <$> next) curr
+  foldM go Nothing $ reverse $ zip [1 ..] preprocessed
   where
-    go t0 curr (remaining, (soln, ans)) = do
-      t <- getCPUTime
-      let remTime = (maxTime - (t - t0)) `div` (remaining * 10 ^ 6)
-      next <-
-        timeout (fromInteger $ min maxTime remTime) $
-        forceEval $ solve (maybe max length curr) soln ans
-      return $ join next <|> curr
+    combine x@(Just (_, _, x')) y@(Just (_, _, y'))
+      | length x' < length y' = x
+      | otherwise = y
+    combine x y = x <|> y
 
-preprocess :: [String] -> [String] -> [([Int], [Maybe (Int, Int)])]
+parsonsBF ::
+     Int -> String -> [String] -> IO (Maybe ([String], [String], [Edit]))
+parsonsBF = parsons solveBF
+
+parsonsDP ::
+     Int -> String -> [String] -> IO (Maybe ([String], [String], [Edit]))
+parsonsDP = parsons solveDP
+
+explain :: [String] -> [String] -> [Edit] -> String
+explain _ _ [] = "no edits"
+explain ans soln edits = unlines $ map (("-\t" ++) . describe) edits
+  where
+    describe (Swap a b) =
+      "swap " ++ trim (soln !! a) ++ " and " ++ trim (soln !! a)
+    describe (Indent a b) =
+      "indent " ++
+      trim (soln !! a) ++
+      if a == b
+        then ""
+        else " through " ++ trim (soln !! b)
+    describe (Move a) = "move " ++ trim (soln !! a) ++ " into place"
+    describe (Insert a) = "insert " ++ trim (soln !! a)
+    describe (Delete a) = "delete " ++ trim (ans !! a)
+    trim s
+      | len < 20 = "'" ++ s ++ "'"
+      | otherwise = "'" ++ take 17 s ++ "...'"
+      where
+        len = length s
+
+preprocess :: [String] -> [String] -> [([String], [Int], [Maybe (Int, Int)])]
 preprocess ans soln =
   let (ans', ansIndents) = indentations ans
       (soln', solnIndents) = indentations soln
       possibleIndexes =
-        zipWith (\i x -> fixup $ map (, i) x) ansIndents $
+        zipWith (\i x -> fix $ map (, i) x) ansIndents $
         map (lookupAll $ zip soln' [0 ..]) ans'
-   in map (solnIndents, ) $
+   in map (soln', solnIndents, ) $
       filter (((==) =<< removeDuplicates) . sort . catMaybes) $
       listProduct possibleIndexes
   where
-    fixup [] = [Nothing]
-    fixup xs = map Just xs
+    fix [] = [Nothing]
+    fix xs = map Just xs
+    indentations strs
+      | scale == 0 = (strs', indents)
+      | otherwise = (strs', map (`div` scale) indents)
+      where
+        (indents, strs') =
+          first (map length) $ unzip $ map (span (`elem` " \t")) strs
+        scale = foldr gcd 0 indents
 
-indentations :: [String] -> ([String], [Int])
-indentations strs =
-  let (indents, strs') = first (map length) $ unzip $ map (span (' ' ==)) strs
-      scale = foldr gcd 0 indents
-   in ( strs'
-      , if scale == 0
-          then indents
-          else map (`div` scale) indents)
+clean :: [Int] -> [Maybe (Int, Int)] -> ([(Int, Int)], [Int], [Int])
+clean soln ans =
+  let (valid, invalid) =
+        partitionEithers $
+        map (uncurry ((`maybe` Left) . Right)) $ zip [0 ..] ans
+      missing = filter (isNothing . (`lookup` valid)) [0 .. length soln - 1]
+   in (valid, invalid, missing)
 
-solve :: Int -> [Int] -> [Maybe (Int, Int)] -> Maybe [Edit]
-solve maxEdits soln ans =
+solveBF :: Int -> [Int] -> [Maybe (Int, Int)] -> Maybe [Edit]
+solveBF maxEdits soln ans =
   (basic ++) . reverse <$>
   go (maxEdits - length basic) [(valid ++ map (, 0) missing, [])]
   where
-    (valid, invalid) =
-      partitionEithers $ map (uncurry ((`maybe` Left) . Right)) $ zip [0 ..] ans
-    missing = filter (isNothing . (`lookup` valid)) [0 .. length soln - 1]
+    (valid, invalid, missing) = clean soln ans
     basic = map Delete invalid ++ map Insert missing
     go remaining states
       | remaining <= 0 = Nothing
@@ -137,10 +190,12 @@ solve maxEdits soln ans =
             go (remaining - 1) $
             removeDuplicates $
             sortBy (compare `on` fst) $
-            concatMap (\(st, es) -> map (second (: es)) $ edits soln st) states
+            concatMap
+              (\(st, es) -> map (second (: es)) $ bfEdits soln st)
+              states
 
-edits :: [Int] -> [(Int, Int)] -> [([(Int, Int)], Edit)]
-edits soln state = swaps ++ indents ++ cycles
+bfEdits :: [Int] -> [(Int, Int)] -> [([(Int, Int)], Edit)]
+bfEdits soln state = swaps ++ indents ++ moves
   where
     len = length state
     swaps =
@@ -161,7 +216,7 @@ edits soln state = swaps ++ indents ++ cycles
       [ ( map snd start ++
           map (second (max 0 . (+ diff))) ((l, i) : map snd toIndent) ++
           map snd end
-        , Indent ix (1 + length toIndent))
+        , Indent ix (last $ ix : map (fst . snd) toIndent))
       | ix <- [0 .. len - 1]
       , let (start, (is, (l, i)):rest) = splitAt ix $ zip soln state
       , l == ix
@@ -171,15 +226,57 @@ edits soln state = swaps ++ indents ++ cycles
       , ix == 0 || not (match $ last start)
       , let (toIndent, end) = span match rest
       ]
-    cycles =
+    moves =
       [ ( insertAt l (l, fromMaybe i (l `lookup` zip [0 ..] soln)) $
           start ++ end
-        , Cycle ix l)
+        , Move ix)
       | ix <- [0 .. len - 2]
       , let (start, (l, i):end) = splitAt ix state
       , ix /= l
       ]
 
+data DPEdit
+  = Keep Int
+  | Indent' [Int]
+  | Move' Int
+  deriving (Eq, Ord, Show)
+
+solveDP :: Int -> [Int] -> [Maybe (Int, Int)] -> Maybe [Edit]
+solveDP _ soln ans =
+  let (valid, invalid, missing) = clean soln ans
+      soln' = zip [0 ..] soln
+      ans' = map (\(l, i) -> (l, fromJust (lookup l soln') - i)) valid
+      edits = snd $ fst $ foldl' go ((0, []), []) ans'
+   in Just $
+      dpEdits ([0 .. length soln - 1] \\ missing) (reverse edits) ++
+      map Delete invalid ++ map Insert missing
+  where
+    go (ans, memo) (line, indent) =
+      let edit
+            | indent == 0 = (1, [Keep line])
+            | otherwise = (0, [Move' line])
+          ans' = maximum $ reverse $ edit : map (next line indent) memo
+       in ((max ans ans'), (line, indent, ans') : memo)
+    next line indent (line', _, (score, edits))
+      | line > line' && indent == 0 = (score + 1, Keep line : edits)
+    next line indent (line', indent', (score, edits))
+      | line > line' && indent == indent' =
+        case edits of
+          (Move' l):edits' -> (score + 1, Indent' [line, l] : edits')
+          (Indent' ls):edits' -> (score + 1, Indent' (line : ls) : edits')
+          _ -> (score, Move' line : edits)
+    next line _ (_, _, (score, edits)) = (score, Move' line : edits)
+
+dpEdits :: [Int] -> [DPEdit] -> [Edit]
+dpEdits soln = go []
+  where
+    go seen [] = map Move (soln \\ seen)
+    go seen (Keep line:rest) = go (line : seen) rest
+    go seen (Indent' lines:rest) =
+      Indent (last lines) (head lines) : go (lines ++ seen) rest
+    go seen (Move' line:rest) = go seen rest
+
+-- * Helpers
 removeDuplicates :: (Eq a) => [(a, b)] -> [(a, b)]
 removeDuplicates ((x, _):xs@((x', _):_))
   | x == x' = removeDuplicates xs
@@ -204,3 +301,6 @@ listProduct (x:xs) = (:) <$> x <*> listProduct xs
 
 forceEval :: (NFData a) => a -> IO a
 forceEval a = evaluate a >>= liftM2 seq rnf return
+
+thd :: (a, b, c) -> c
+thd (_, _, x) = x
